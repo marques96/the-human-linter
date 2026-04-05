cb69fba2b0141b04ac3277ce6665c65ead3acf25
Matheus Marques <marques.mdom7@gmail.com>
2026-04-05 19:42:45 -0400

    Separando funções

--- calcularMedia.py
+++ calcularMedia.py
@@ -1,13 +1,4 @@
 def calcular_media(numeros):
-    # ERRO 1 (lógica): soma / (len+1) ao invés de len
     total = sum(numeros)
     media = total / (len(numeros) + 1)  
     return media
-
-# ERRO 2 (estilo/PEP8) - falta espaços após vírgula e nome confuso
-def mediaPonderada(notas,pesos):
-    soma=0
-    for i in range(len(notas)):
-        soma+=notas[i]*pesos[i]
-    return soma/sum(pesos)
-EOF
