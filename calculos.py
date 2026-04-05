2a4f20f4f523a8c39fe66bb0611fd2d9cf086d1e
Matheus Marques <marques.mdom7@gmail.com>
2026-04-05 19:11:44 -0400

    Função com erros propositais

---
+++ calculos.py
@@ -0,0 +1,13 @@
+def calcula_media(numeros):
+    # ERRO 1 (lógica): soma / (len+1) ao invés de len
+    total = sum(numeros)
+    media = total / (len(numeros) + 1)  
+    return media
+
+# ERRO 2 (estilo/PEP8) - falta espaços após vírgula e nome confuso
+def mediaPonderada(notas,pesos):
+    soma=0
+    for i in range(len(notas)):
+        soma+=notas[i]*pesos[i]
+    return soma/sum(pesos)
+EOF
